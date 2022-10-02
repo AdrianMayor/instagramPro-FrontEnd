53894a8d0609739619c8944c9fc44897764c3cdf
Adrian Mayor <adrianmayorsantana@gmail.com>
2022-10-02 15:00:00 +0200

    working on loaders

--- src/components/ImagesBox/ImagesBox.jsx
+++ src/components/ImagesBox/ImagesBox.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import SpinnerIcon from "../../assets/icons/loadingIcon.svg";
+import LoadingPhoto from "../../assets/icons/loadingPhoto.svg";
 import Carousel, { CarouselItem } from "../Carousel/Carousel";
 
 export const ImagesBox = ({ photos, entryOwnerUsername }) => {
   const [photoLoading, setPhotoLoading] = useState(true);
-  const [count, setCount] = useState(0);
 
   return (
     photos.length >= 1 && (
@@ -14,13 +13,12 @@ export const ImagesBox = ({ photos, entryOwnerUsername }) => {
             <img
               src={
                 photoLoading
-                  ? `${SpinnerIcon}`
+                  ? `${LoadingPhoto}`
                   : `${process.env.REACT_APP_SERVER}/${photo.imageName}`
               }
               alt={`Post from ${entryOwnerUsername}`}
               onLoad={() => {
                 setPhotoLoading(false);
-                setCount(count + 1);
               }}
             ></img>
           </CarouselItem>
