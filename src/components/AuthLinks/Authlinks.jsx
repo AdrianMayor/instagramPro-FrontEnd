c47dda03f28a7e652f7638975c96dd2607a95d52
Adrian Mayor <adrianmayorsantana@gmail.com>
2022-10-04 18:03:43 +0200

    correcting text

--- src/components/AuthLinks/Authlinks.jsx
+++ src/components/AuthLinks/Authlinks.jsx
@@ -1,69 +1,69 @@
-import './Authlinks.css';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../context/authContext';
-import { useContext } from 'react';
-import { PhotoProfile } from '../PhotoProfile/PhotoProfile';
-import { LogoutIcon } from '../LogoutIcon/LogoutIcon';
-import { SearchBar } from '../SearchBar/SearchBar';
-import { HomeLink } from '../HomeLink/HomeLink';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import { HomeLink } from "../HomeLink/HomeLink";
+import { LogoutIcon } from "../LogoutIcon/LogoutIcon";
+import { PhotoProfile } from "../PhotoProfile/PhotoProfile";
+import { SearchBar } from "../SearchBar/SearchBar";
+import "./Authlinks.css";
 
+export const AuthLinks = ({
+  link1,
+  link2,
+  handleClicker,
+  handleClickRemove,
+}) => {
+  const { user, logout } = useContext(AuthContext);
 
-export const AuthLinks = ({ link1, link2, handleClicker, handleClickRemove }) => {
-
-    const { user, logout } = useContext(AuthContext);
-
-    return (
-        <>
-            <nav className='header-nav'>
-                {user ? (
-                    <ul className='head'>
-                        <li className='head-user'>
-                            <PhotoProfile user={user} />
-                            <p>
-                                {user.username}
-                            </p>
-                        </li>
-                        <li className='container-logout-icon'>
-                            <Link to='/login' onClick={handleClicker}>
-                                <button className='logout-button' onClick={() => logout()}>
-                                    <LogoutIcon />
-                                </button>
-                            </Link>
-                        </li>
-                        <li className='container-home-icon'>
-                            <Link to='/' onClick={handleClickRemove}>
-                                <HomeLink />
-                            </Link>
-                        </li>
-                    </ul>
-                ) : (
-                    <ul className='head-noaccount'>
-                        <li className='container-register-link'>
-                            <Link to='/register' className='register-link'>
-                                <p id='register' onClick={handleClicker} className={link1}>
-                                    Singup
-                                    <span className='underline'></span>
-                                </p>
-                            </Link>
-                        </li>
-                        <li className='container-home-icon-noaccount'>
-                            <Link to='/' onClick={handleClickRemove}>
-                                <HomeLink />
-                            </Link>
-                        </li>
-                        <li className='container-login-link'>
-                            <Link to='/login' className='login-link'>
-                                <p id='login' onClick={handleClicker} className={link2}>
-                                    Login
-                                    <span className='underline'></span>
-                                </p>
-                            </Link>
-                        </li>
-                    </ul>
-                )}
-            </nav>
-            <SearchBar />
-        </>
-
-    )
-}
+  return (
+    <>
+      <nav className="header-nav">
+        {user ? (
+          <ul className="head">
+            <li className="head-user">
+              <PhotoProfile user={user} />
+              <p>{user.username}</p>
+            </li>
+            <li className="container-logout-icon">
+              <Link to="/login" onClick={handleClicker}>
+                <button className="logout-button" onClick={() => logout()}>
+                  <LogoutIcon />
+                </button>
+              </Link>
+            </li>
+            <li className="container-home-icon">
+              <Link to="/" onClick={handleClickRemove}>
+                <HomeLink />
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          <ul className="head-noaccount">
+            <li className="container-register-link">
+              <Link to="/register" className="register-link">
+                <p id="register" onClick={handleClicker} className={link1}>
+                  Sign Up
+                  <span className="underline"></span>
+                </p>
+              </Link>
+            </li>
+            <li className="container-home-icon-noaccount">
+              <Link to="/" onClick={handleClickRemove}>
+                <HomeLink />
+              </Link>
+            </li>
+            <li className="container-login-link">
+              <Link to="/login" className="login-link">
+                <p id="login" onClick={handleClicker} className={link2}>
+                  Login
+                  <span className="underline"></span>
+                </p>
+              </Link>
+            </li>
+          </ul>
+        )}
+      </nav>
+      <SearchBar />
+    </>
+  );
+};
