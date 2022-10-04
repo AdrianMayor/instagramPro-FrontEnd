48d8fba1cd8b6864bcdc69b785e5f1ed2748a2cd
Adrian Mayor <adrianmayorsantana@gmail.com>
2022-10-04 14:09:13 +0200

    working on styles , transitions and ErrorPage when there is no post

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 import { CommentBox } from "../CommentBox/CommentBox";
 import { ImagesBox } from "../ImagesBox/ImagesBox";
 import { LikeAndCommentMenu } from "../LikeAndCommentMenu/LikeAndCommentMenu";
@@ -9,6 +10,7 @@ import "./PostCard.css";
 export const PostCard = ({ post, singlePost, token }) => {
   const [isSinglePost, setSinglePost] = useState(false);
   const [newCommentToggle, setNewCommentToggle] = useState(false);
+  const nodeRef = useRef(null);
 
   /* Componente que dependiendo de si es una vista individual o multiple cambia su comportamiento */
   useEffect(() => {
@@ -74,8 +76,14 @@ export const PostCard = ({ post, singlePost, token }) => {
             />
           </div>
 
-          <footer className="postCard__commentBox">
-            {newCommentToggle && (
+          <CSSTransition
+            in={newCommentToggle}
+            nodeRef={nodeRef}
+            classNames="postCard__commentTransition"
+            timeout={300}
+            unmountOnExit
+          >
+            <footer ref={nodeRef} className="postCard__commentBox">
               <CommentBox
                 comments={post.comments}
                 idEntry={post.entryId}
@@ -83,8 +91,8 @@ export const PostCard = ({ post, singlePost, token }) => {
                 singlePost={isSinglePost}
                 token={token}
               />
-            )}
-          </footer>
+            </footer>
+          </CSSTransition>
         </article>
       </>
     )
--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { PostCard } from "../components/PostCard/PostCard";
 import { AuthContext } from "../context/authContext";
+import { NotFoundPage } from "../pages/ErrorPage";
 import { services } from "../services";
 
 export const SinglePost = () => {
@@ -30,9 +31,12 @@ export const SinglePost = () => {
     getEntry();
   }, [entryId]);
 
-  return (
-    singlePost !== undefined && (
-      <PostCard post={singlePost} token={token} singlePost={true}></PostCard>
-    )
+  return singlePost !== undefined ? (
+    <PostCard post={singlePost} token={token} singlePost={true}></PostCard>
+  ) : (
+    <NotFoundPage
+      error="No Results"
+      className="singleEntryNotFound"
+    ></NotFoundPage>
   );
 };
